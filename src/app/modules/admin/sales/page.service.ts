83c1a3e2f5786728f04f259e39adb25e1c91ae66
narongkorn-kevin <narongkorn.kevin@gmail.com>
2025-03-05 09:12:53 +0700

    add updateStatus to service

--- src/app/modules/admin/sales/page.service.ts
+++ src/app/modules/admin/sales/page.service.ts
@@ -135,4 +135,15 @@ export class PageService {
                 })
             );
     }
+
+    updateStatus(data: any): Observable<any> {
+        return this._httpClient
+            .post<any>(environment.baseURL + '/api/update_status_order/', data)
+            .pipe(
+                tap((result) => {
+                    this._data.next(result);
+                })
+            );
+    }
+
 }
